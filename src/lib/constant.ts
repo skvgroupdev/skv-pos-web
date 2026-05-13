c5ea762f6369de0d3a9e56b478470b40a2e62878
souvls <soulixai.vls@gmail.com>
2026-05-13 10:15:36 +0700

    fix: update API_BASE_URL to point to the correct production endpoint

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,4 @@
 export const S3_BUCKET_URL = "https://skvgroupbucket.s3.ap-southeast-1.amazonaws.com/pos/";
 export const ENVIRONMENT = "development";
-// export const API_BASE_URL = "https://api-pos.skvlao.com/api";
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "https://api-pos.skvlao.com/api";
+// export const API_BASE_URL = "http://localhost:8000/api";
